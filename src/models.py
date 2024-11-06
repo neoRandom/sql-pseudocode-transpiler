993fc51ca71aec052f5e33b760b0f682553887ea
rakRandom <fellipeleo100@gmail.com>
2024-11-06 03:09:58 -0300

    refactor: move models to the proper file

---
+++ src/models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import List
+
+class Attribute(BaseModel):
+    name: str
+    type: str
+    size: str
+    modifiers: List[str]
+
+class Table(BaseModel):
+    name: str
+    attribute_list: List[Attribute]
+
+class DatabaseSchema(BaseModel):
+    database_name: str
+    table_list: List[Table]
+
+class JSONSchema(BaseModel):
+    name: str
+    size: int
+    body: List[Attribute]
